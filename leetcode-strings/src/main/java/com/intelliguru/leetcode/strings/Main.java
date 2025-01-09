1fcd8ce848b2204a0b9f4826c46dc85352e41afe
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-10 01:06:23 +0530

    commit 2

---
+++ leetcode-strings/src/main/java/com/intelliguru/leetcode/strings/Main.java
@@ -0,0 +1,7 @@
+package com.intelliguru.leetcode.strings;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO FROM LeetCode String Problems.");
+    }
+}
